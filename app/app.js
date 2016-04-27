fef0707d183caef4282492f9962098a0a60c9ce0
vrajakishore <vrajakishore2@gmail.com>
2016-04-27 13:30:21 +0530

    temp folder to checking temporary modifications

--- app/app.js
+++ app/app.js
@@ -11,71 +11,17 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
 
     // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'pages/home.html'
+        })
+
         .state('events', {
             url: '/events',
             templateUrl: 'pages/events.html'
         })
 
 
-        // nested list with custom controller and add actor
-        .state('create.addActor', {
-            url: '/addActor',
-            templateUrl: 'pages/create/createActor.html',
-            /*controller: function($scope) {
-             $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-             }*/
-        })
-
-        // nested list with add film
-        .state('create.addFilm', {
-            url: 'addFilm',
-            templateUrl: 'pages/create/createFilm.html'
-
-        })
-
-        // UPDATE STATE AND NESTED VIEWS =====================================
-        .state('update', {
-            url: '/update',
-            templateUrl: 'pages/update/update.html'
-
-        })
-
-        // nested list with update actor
-        .state('update.updateActor', {
-            url: '/updateActor',
-            templateUrl: 'pages/update/updateActor.html'
-        })
-
-        // nested list with update film
-        .state('update.updateFilm', {
-            url: 'updateFilm',
-            templateUrl: 'pages/update/updateFilm.html'
-
-        })
-
-        // SEARCH STATE AND NESTED VIEWS =====================================
-        .state('search', {
-            url: '/search',
-            templateUrl: 'pages/search/search.html'
-        })
-
-        .state('search.searchActor', {
-            url: '/searchActor',
-            templateUrl: 'pages/search/searchActor.html',
-            controller:"findActor"
-        })
-
-        .state('search.searchFilm', {
-            url: '/searchFilm',
-            templateUrl: 'pages/search/searchFilm.html',
-            controller:"findFilm"
-        })
-
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('register', {
-            url: '/register',
-            templateUrl: 'register.html'
-        });
        
 
 });
