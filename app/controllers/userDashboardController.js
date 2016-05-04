25cf5c56be56c62f057f40febefd95ea36b2b32c
dimplesiyal <dimplesiyal@gmail.com>
2016-05-04 10:29:54 +0530

    conditions

--- app/controllers/userDashboardController.js
+++ app/controllers/userDashboardController.js
@@ -6,37 +6,37 @@
 routerApp.controller('userDashboardController',function($scope,$state,$location,$stateParams,$rootScope,$http,$window) {
 
 //Account Settings
-        if($rootScope.isLogin){
+        if($rootScope.isLogin) {
 
-            $scope.changePassword= function(Details){
+            $scope.changePassword = function (Details) {
 
                 console.log(Details);
 
-            }}
+            }
 
 
-        $scope.editProfile= function(Details){
+            $scope.editProfile = function (Details) {
 
-            console.log(Details);
+                console.log(Details);
 
-        }
-        $scope.deactivateAcc= function(Details){
+            }
+            $scope.deactivateAcc = function (Details) {
 
-            console.log(Details);
+                console.log(Details);
 
-        }
+            }
 //Event Management
-    $scope.createEvent= function(Details){
+            $scope.createEvent = function (Details) {
 
-             console.log(Details);
+                console.log(Details);
+            }
+
+            $scope.modifyEvent = function (Details) {
+
+                console.log(Details);
+            }
+            $scope.removeEvent = function (Details) {
+                console.log(Details);
+            }
         }
-
-$scope.modifyEvent= function(Details){
-
-        console.log(Details);
-   }
-    $scope.removeEvent= function(Details) {
-        console.log(Details);
-    }
-
 });
